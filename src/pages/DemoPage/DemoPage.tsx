ba7aef10f29df3cfccab94e9f37630459e3ca837
Spandan Barve <spandan567@gmail.com>
2023-11-19 12:23:36 +0530

    load demo

--- src/pages/DemoPage/DemoPage.tsx
+++ src/pages/DemoPage/DemoPage.tsx
@@ -4,6 +4,22 @@ export default function DemoPage() {
   return (
     <>
       <Hero />
+
+      <div className="relative my-10">
+        <iframe
+          className="aspect-video w-full"
+          src="https://cal.com/blockpeer/30min?date=2023-11-15&month=2023-11"
+        />
+
+        <div className="absolute-cover flex justify-center items-center -z-1">
+          <div className="flex flex-col items-center gap-y-8">
+            <div className="animate-spin border-[5px] border-dashed border-primary aspect-square w-[20vw] rounded-full" />
+            <p className="text-primary">Loading ...</p>
+          </div>
+        </div>
+
+        <div className="bg-background absolute z-10 top-0 left-0 w-full h-[4vw]" />
+      </div>
     </>
   );
 }
--- src/pages/DemoPage/components/Hero.tsx
+++ src/pages/DemoPage/components/Hero.tsx
@@ -15,15 +15,6 @@ export default function Hero() {
           </p>
         </div>
       </div>
-      <div className="relative my-10">
-        <iframe
-          className="aspect-video w-full"
-          src="https://cal.com/blockpeer/30min?date=2023-11-15&month=2023-11"
-        />
-
-        <div className="bg-background absolute z-10 top-0 left-0 w-full h-[4vw]" />
-        {/* <div className="bg-background absolute z-10 bottom-0 left-0 w-full h-[10vw]" /> */}
-      </div>
     </section>
   );
 }
